37747e28fe7ff47a70833adc4a90b4e261c11f5a
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-12 16:39:53 -0800

    Robust cleanup

--- src/lib/installGitRepo.ts
+++ src/lib/installGitRepo.ts
@@ -15,47 +15,66 @@ const dist = path.join(__dirname, '..', '..');
 const version = major > 14 ? 'local' : 'stable';
 const workerWrapper = wrapWorker(path.join(dist, 'cjs', 'lib', 'installGitRepo.js'));
 
-function worker(repo, dest, options: CommandOptions | InstallOptions, callback: CommandCallback) {
-  const installOptions = options as InstallOptions;
-  // options.clean = true;
-  function checkOrClean(dest, callback) {
-    installOptions.clean ? safeRm(dest, () => callback(new Error('clone'))) : fs.stat(dest, callback);
-  }
-
-  checkOrClean(dest, (err) => {
-    const queue = new Queue(1);
-    queue.defer(mkdirp.bind(null, dest));
-
-    // does not exist - clone
-    if (err) {
-      queue.defer(spawn.bind(null, 'git', ['clone', repo, path.basename(dest)], { cwd: path.dirname(dest), stdio: 'inherit' }));
-    }
-    // exists - reset git
-    else {
-      // Remove stale lock files first
-      queue.defer((cb) => {
-        const gitDir = path.join(dest, '.git');
-        try {
-          const files = fs.readdirSync(gitDir);
-          for (let i = 0; i < files.length; i++) {
-            if (files[i].indexOf('.lock') >= 0) {
-              try { fs.unlinkSync(path.join(gitDir, files[i])); } catch (_e) { /* ignore */ }
-            }
-          }
-        } catch (_e) { /* ignore */ }
-        cb();
-      });
-      // Abort any in-progress merge/rebase (ignore errors if none in progress)
-      queue.defer((cb) => spawn('git', ['merge', '--abort'], { cwd: dest, stdio: 'inherit' }, () => cb()));
-      queue.defer((cb) => spawn('git', ['rebase', '--abort'], { cwd: dest, stdio: 'inherit' }, () => cb()));
-      queue.defer(spawn.bind(null, 'git', ['clean', '-fd'], { cwd: dest, stdio: 'inherit' }));
-      queue.defer(spawn.bind(null, 'git', ['reset', '--hard', 'HEAD'], { cwd: dest, stdio: 'inherit' }));
-      queue.defer(spawn.bind(null, 'git', ['pull', '--rebase'], { cwd: dest, stdio: 'inherit' }));
-    }
-
-    queue.defer(spawn.bind(null, 'npm', ['install', '--silent'], { cwd: dest }));
-    queue.await(callback);
+function checkDirectoryExists(dest: string, callback: (err: Error | null, exists?: boolean) => void) {
+  fs.stat(dest, (err) => {
+    if (err && err.code === 'ENOENT') callback(null, false);
+    else if (err) callback(err);
+    else callback(null, true);
   });
+}
+
+function cloneRepository(repo: string, dest: string, callback) {
+  const parentDir = path.dirname(dest);
+  const repoName = path.basename(dest);
+  const queue = new Queue(1);
+  queue.defer(mkdirp.bind(null, dest));
+  queue.defer(spawn.bind(null, 'git', ['clone', repo, repoName], { cwd: parentDir, stdio: 'inherit' }));
+  queue.await(callback);
+}
+
+function updateRepository(dest: string, callback) {
+  const queue = new Queue(1);
+  queue.defer(spawn.bind(null, 'git', ['clean', '-fd'], { cwd: dest, stdio: 'inherit' }));
+  queue.defer(spawn.bind(null, 'git', ['reset', '--hard', 'HEAD'], { cwd: dest, stdio: 'inherit' }));
+  queue.defer(spawn.bind(null, 'git', ['pull', '--rebase'], { cwd: dest, stdio: 'inherit' }));
+  queue.await(callback);
+}
+
+function installDependencies(dest: string, callback) {
+  spawn('npm', ['install', '--silent'], { cwd: dest }, callback);
+}
+
+function cleanInstall(repo: string, dest: string, callback) {
+  const queue = new Queue(1);
+  queue.defer(safeRm.bind(null, dest));
+  queue.defer(cloneRepository.bind(null, repo, dest));
+  queue.defer(installDependencies.bind(null, dest));
+  queue.await(callback);
+}
+
+function worker(repo: string, dest: string, options: CommandOptions | InstallOptions, callback: CommandCallback) {
+  const installOptions = options as InstallOptions;
+
+  if (installOptions.clean) {
+    cleanInstall(repo, dest, callback);
+  } else {
+    checkDirectoryExists(dest, (err, exists) => {
+      if (err) return callback(err);
+      const queue = new Queue(1);
+      if (!exists) {
+        queue.defer(cloneRepository.bind(null, repo, dest));
+        queue.defer(installDependencies.bind(null, dest));
+      } else {
+        queue.defer((cb) => {
+          updateRepository(dest, (err2) => {
+            if (err2) return cleanInstall(repo, dest, cb);
+            installDependencies(dest, cb);
+          });
+        });
+      }
+      queue.await(callback);
+    });
+  }
 }
 
 export default function installGitRepo(repo: string, dest: string, options: CommandOptions | CommandCallback, callback?: CommandCallback): undefined {
